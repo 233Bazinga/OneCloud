4ee8e1727eea88a338947dddc6feb5eaf711053e
233Bazinga <52889695+233Bazinga@users.noreply.github.com>
2021-06-26 19:21:46 +0800

    Add files via upload

--- mod.sh
+++ mod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) P3TERX <https://p3terx.com>
 #
 
 # 配置自定义主题
@@ -15,18 +15,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # 集成自定义插件
 #
-# > AdguardHome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/community/luci-app-adguardhome
-#
-# > Dockerman
-#git clone https://github.com/lisaac/luci-lib-docker package/community/luci-lib-docker
-#git clone https://github.com/lisaac/luci-app-dockerman package/community/luci-app-dockerman
-#
 # > DnsFilter
 git clone https://github.com/garypang13/luci-app-dnsfilter package/community/luci-app-dnsfilter
 #
 # > GoWebDav
-#git clone https://github.com/project-openwrt/openwrt-gowebdav package/community/openwrt-gowebdav
+# git clone https://github.com/immortalwrt/openwrt-gowebdav package/community/openwrt-gowebdav
 #
 # > HelloWorld (vssr)
 git clone https://github.com/jerrykuku/lua-maxminddb package/community/lua-maxminddb
@@ -42,21 +35,23 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/c
 #sed -i '$i '"sed -i '/luciname/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
 #sed -i '$i '"echo 'luciname = \"Limitless\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i '"sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i '"echo 'luciversion = \"w8ves\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i '"echo 'luciversion = \"Mod\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
 #
 # > 修改插件位置
 sed -i 's/\"services\"/\"network\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
-#sed -i '/\"NAS\"/d' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
-#sed -i 's/\"nas\"/\"services\"/g' package/lean/luci-app-rclone/luasrc/controller/rclone.lua
-#sed -i '/\"NAS\"/d' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
-#sed -i 's/\"nas\"/\"services\"/g' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
+sed -i 's/\"system\"/\"services\"/g' package/lean/luci-app-ttyd/luasrc/controller/terminal.lua
+sed -i '/\"NAS\"/d' package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
+sed -i 's/\"vpn\"/\"services\"/g' package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
+# sed -i '/\"NAS\"/d' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
+# sed -i 's/\"nas\"/\"services\"/g' package/community/openwrt-gowebdav/luci-app-gowebdav/luasrc/controller/gowebdav.lua
 #
 # > 修改插件名字
-#sed -i 's/\"Rclone\"/\"RcloneSYNC\"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/\"CPU 使用率（%）\"/\"CPU负载\"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/\"DDNS.to内网穿透\"/\"DDNS.TO\"/g' feeds/nas/luci/luci-app-ddnsto/po/zh-cn/ddnsto.po
+sed -i 's/\"Docker CE 容器\"/\"DockerCE\"/g' package/lean/luci-app-docker/po/zh-cn/docker.po
+sed -i 's/\"TTYD 终端\"/\"Terminal\"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/\"Argon 主题设置\"/\"主题设置\"/g' package/community/luci-app-argon-config/po/zh-cn/argon-config.po
-sed -i 's/\"TTYD 终端\"/\"网页终端\"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/\"解锁网易云灰色歌曲\"/\"NeteaseMusic\"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
-sed -i 's/\"KMS 服务器\"/\"KMS Activator\"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
+#sed -i 's/\"KMS 服务器\"/\"KMS Activator\"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 sed -i 's/\"Turbo ACC 网络加速\"/\"网络加速\"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 #
